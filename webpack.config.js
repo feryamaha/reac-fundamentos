f74442adfb6aa7c267307e3d00727439d5dfbb77
Fernando Moreira <feryamaha@hotmail.com>
2025-04-08 17:31:21 -0300

    Ajuste no webpack e dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle[hash].js',
-        publicPath: '/'
+        publicPath: '/reac-fundamentos/'
     },
 
     plugins: [
